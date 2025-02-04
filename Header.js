aae6f1633a8975ebfc4f7a5ec3ef84aff343fed5
EswaraSatya <eswara.satya@ideapoke.com>
2025-02-04 17:18:11 +0530

    Button updated

--- Header.js
+++ Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 const Header = ({ title, content, className }) => {
+
+    const clickHandler = () => {
+        alert("Clicked on button")
+    }
+
     return (
         <div className={className}>
             <h2>{title}</h2>
             <p>{content}</p>
+            <button onClick={clickHandler}></button>
         </div>
     );
 };
